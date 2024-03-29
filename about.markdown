154d6b426d2d2b9ed50115f215f1c2d355e892d1
dtingey <damon.tingey@gmail.com>
2024-03-29 13:09:32 -0600

    initial svelte commit

--- about.markdown
+++
@@ -1,7 +0,0 @@
----
-layout: page 
-title: About
-permalink: /about/
----
-
-My name is Damon Tingey. This is a work in progress. Here is the [home page](https://dtingey.github.io)
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
+
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        })
+    }
+};
+
+export default config;
